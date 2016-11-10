bf6bc14d033a9d3256398151d06923f3a447dd72
Bhavik Jain <bhavikjain358@gmail.com>
2016-11-10 00:31:11 -0500

    fb5

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './components/feed';
 ReactDOM.render(
-  <Feed />,
-  document.getElementById('fb-feed')
+	<Feed user={4} />,
+	document.getElementById('fb-feed')
 );
